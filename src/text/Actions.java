57f4f1d8b1e5447185e720b3c41375dc73cdb75b
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2016-02-26 09:39:29 -0300

    Find

--- src/text/Actions.java
+++ src/text/Actions.java
@@ -1,8 +1,13 @@
 package text;
 
+import javax.swing.JOptionPane;
+
 public class Actions {
 
 	Notepad n;
+	
+	String findWord;
+	int findIndex;
 	
 	public Actions(Notepad n) {
 		this.n = n;
@@ -19,4 +24,15 @@ public class Actions {
 	 public void paste(){
 	     n.getTextComponent().toString();
 	}
+	 
+	public void find() {
+		findWord = JOptionPane.showInputDialog("Type the word to find");
+		findIndex = n.getTextComponent().indexOf(findWord);
+		if (findIndex == -1) {
+			JOptionPane.showMessageDialog(null,"Word not found");
+		} else { 
+			// selectFound(); 
+		}			
+	}
+	 
 }
